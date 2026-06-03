40cde69c051a2963e391441bac080d995a018c36
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-03 20:22:35 +0000

    Automated update

--- src/Soenneker.Replicate.OpenApiClient/ModelsRequests/ModelsRequestBuilder.cs
+++ src/Soenneker.Replicate.OpenApiClient/ModelsRequests/ModelsRequestBuilder.cs
@@ -35,7 +35,7 @@ namespace Soenneker.Replicate.OpenApiClient.ModelsRequests
         /// </summary>
         /// <param name="pathParameters">Path parameters for the request</param>
         /// <param name="requestAdapter">The request adapter to use to execute the requests.</param>
-        public ModelsRequestBuilder(Dictionary<string, object> pathParameters, IRequestAdapter requestAdapter) : base(requestAdapter, "{+baseurl}/models{?sort_by*,sort_direction*}", pathParameters)
+        public ModelsRequestBuilder(Dictionary<string, object> pathParameters, IRequestAdapter requestAdapter) : base(requestAdapter, "", pathParameters)
         {
         }
         /// <summary>
@@ -43,7 +43,7 @@ namespace Soenneker.Replicate.OpenApiClient.ModelsRequests
         /// </summary>
         /// <param name="rawUrl">The raw URL to use for the request builder.</param>
         /// <param name="requestAdapter">The request adapter to use to execute the requests.</param>
-        public ModelsRequestBuilder(string rawUrl, IRequestAdapter requestAdapter) : base(requestAdapter, "{+baseurl}/models{?sort_by*,sort_direction*}", rawUrl)
+        public ModelsRequestBuilder(string rawUrl, IRequestAdapter requestAdapter) : base(requestAdapter, "", rawUrl)
         {
         }
         /// <summary>
